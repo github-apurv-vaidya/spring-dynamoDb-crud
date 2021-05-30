2ec08477ecfa7f980d69ab52f6b64f4dc01ff7ec
apurv <a.k.vaidya62@gmail.com>
2021-05-31 02:23:05 +0530

    removed sensitive information

--- src/main/java/com/aws/dynamodb/crud/util/Constants.java
+++ src/main/java/com/aws/dynamodb/crud/util/Constants.java
@@ -2,7 +2,7 @@ package com.aws.dynamodb.crud.util;
 
 public class Constants {
 
-	public final static String ACCESS_KEY = "AKIA2PTM5E4H43PTK7JW";
+	public final static String ACCESS_KEY = "";
 	public final static String REGION = "us-east-1";
-	public final static String SECRET_KEY = "mfFmfW2NzcTSFFhKuTTiOkibXOMdEsMWC3ZDQ5NQ";
+	public final static String SECRET_KEY = "";
 }
